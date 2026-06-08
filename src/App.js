ac1f5640525395cb9ff29076c2bf4c5aa02c2bee
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-06-08 20:58:09 +0530

    class components finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './components/Greet';
+import Welcome from './components/Wekcome';
+
 
 function App() {
   return (
     <div className="App">
       <FirstComponent />
+      <Welcome />
     </div>
   );
 }
